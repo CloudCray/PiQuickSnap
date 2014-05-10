de11f3c282a4cf01512f0bba8dae0e211c670188
Cloud Cray <cloudcray@gmail.com>
2014-05-10 20:10:22 +0000

    Added access token to uploader

--- upload.py
+++ upload.py
@@ -1,11 +1,15 @@
 import os
 import pyimgur
 
-from my_settings import CLIENT_ID
+from my_settings import CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN, REFRESH_TOKEN
 
 def upload_image(filename, title=None):
-	im = pyimgur.Imgur(CLIENT_ID)
+	im = pyimgur.Imgur(CLIENT_ID, 
+			client_secret=CLIENT_SECRET,
+			access_token=ACCESS_TOKEN, 
+			refresh_token=REFRESH_TOKEN)
 	uploaded_image = im.upload_image(filename, title)
 	return uploaded_image
 
 
+#  from authorize import new_access_token
